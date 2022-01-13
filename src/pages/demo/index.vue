32ed64b0291feea4ffed74984f4d998dcc2fc738
TerryMin <15896938825@163.com>
2022-01-13 11:32:00 +0800

    delete build

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<picker mode="date" :value="date" start="2015-09-01" end="2020-09-01" @change="bindDateChange">
+			<view class="picker">
+				当前选择: {{date}}
+			</view>
+		</picker>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				date: new Date()
+			};
+		}
+	}
+</script>
+
+<style>
+</style>
